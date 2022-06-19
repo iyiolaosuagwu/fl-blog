ba5767de58e4496e9758603795cd6d4259bb4865
iyiola <iyiolaosuagwu1@gmail.com>
2022-06-19 17:53:10 +0100

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fl-blog/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
